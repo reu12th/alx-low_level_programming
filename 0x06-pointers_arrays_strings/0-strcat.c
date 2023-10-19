4d7fc17c152e674e17e72120fb1f83f9474b9f82
Reuben Abdul <reu12th@gmail.com>
2023-10-20 00:00:25 +0100

    0. strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,7 +3,7 @@
 /**
  * _strcat - a function that concatenates two strings.
  * @dest: destination string
- * @src: source string 
+ * @src: source string
  * Return: dest
  */
 char *_strcat(char *dest, char *src)
